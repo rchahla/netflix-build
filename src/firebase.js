fab9ee04a33c259891a0bdc14d241f9d8a7e2260
rchahla <rchahla2@uwo.ca>
2025-06-11 18:44:49 -0400

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCeGidFJ0SUsY1DdKuWY3uzxhrfDaOc_Yk",
+  authDomain: "netflix-clone-32c7a.firebaseapp.com",
+  projectId: "netflix-clone-32c7a",
+  storageBucket: "netflix-clone-32c7a.firebasestorage.app",
+  messagingSenderId: "88073492339",
+  appId: "1:88073492339:web:fe963c4a0f65ef55ec1706",
+};
