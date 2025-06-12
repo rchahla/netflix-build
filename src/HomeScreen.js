d80f35dbab6a85af973aeaa5dd4e7fa73f631206
rchahla <rchahla2@uwo.ca>
2025-06-11 20:13:07 -0400

    Set up the navigation bar lay-out and placed the netflix logo and the netflix avatar in the proper place

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+function HomeScreen() {
+  return (
+    <div className="homescreen">
+      <Nav />
+
+      {/*Banner*/}
+
+      {/*Rows*/}
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav nav_black">
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt=""
+        />
+
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
