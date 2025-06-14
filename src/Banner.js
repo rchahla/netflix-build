e210aae60089bd34a978ba604b029b5bc186031f
rchahla <rchahla2@uwo.ca>
2025-06-14 16:13:08 -0400

    Started working on the homepage banner

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+
   return (
     <header
       className="banner"
@@ -17,7 +21,13 @@ function Banner() {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">This is a test description</h1>
+        <h1 className="banner__description">
+          {truncate(
+            `This is a test description This is a test description This is a test description
+            This is a test description This is a test description This is a test description`,
+            150
+          )}
+        </h1>
       </div>
 
       <div className="banner--fadeBottom" />
