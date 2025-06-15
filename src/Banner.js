d1fe509e6840cd33013929e6043cc9a7a7cce90c
rchahla <rchahla2@uwo.ca>
2025-06-14 22:55:50 -0400

    Fully finished the banner, I added dynamically chaning banner backdrop images and their corresponding movie/tvshow name as well as their description

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -11,22 +31,20 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://images.squarespace-cdn.com/content/v1/5efce5920d28887981c5bd9b/1620325755456-HV7G0OW366L9OEPUPR2C/Lupin+Part+2+banner.jpg")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner_contents">
-        <h1 className="banner_title">Movie Name</h1>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `This is a test description This is a test description This is a test description
-            This is a test description This is a test description This is a test description`,
-            150
-          )}
+          {truncate(`${movie?.overview}`, 150)}
         </h1>
       </div>
 
